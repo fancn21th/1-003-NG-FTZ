4b217603bee4b7866d2db5508787edbf18cc3478
fancn <fanyj@yikeer.com>
2019-09-07 19:27:00 +0800

    feature: side nav

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatIconModule } from "@angular/material/icon";
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule
+  ]
+})
+export class MaterialModule {}
